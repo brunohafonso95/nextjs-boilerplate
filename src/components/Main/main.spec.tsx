20d87848c5ed25e50d8377e14924b1552afcec7a
Bruno Afonso <brunohafonso@gmail.com>
2020-11-07 15:27:07 -0300

    test: add test to validate the component style properties

--- src/components/Main/main.spec.tsx
+++ src/components/Main/main.spec.tsx
@@ -6,8 +6,15 @@ describe('Main Component Unit Tests', () => {
   it('should render the heading', () => {
     const { container } = render(<Main />);
     expect(
-      screen.getByRole('heading', { name: /react avançado/i })
+      screen.getByRole('heading', {
+        name: /Typescript, NextJS e Styled Components/i,
+      })
     ).toBeInTheDocument();
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />);
+    expect(container.firstChild).toHaveStyle({ background: '#06092b' });
   });
 });
