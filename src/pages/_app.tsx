609b770055f5afac9c34877fd5e22ca82715f58f
Bruno Afonso <brunohafonso@gmail.com>
2020-11-07 18:24:12 -0300

    feat: add pwa support on project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,9 @@ function App({ Component, pageProps }: AppProps) {
         <title>React Avançado - Boilerplate</title>
         <link rel="shortcut icon" href="/assets/icon-512.png" />
         <link rel="apple-touch-icon" href="/assets/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
+        <link rel="mask-icon" href="/assets/icon-512.png" color="#06092B" />
         <meta
           name="description"
           content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
