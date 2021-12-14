c5eee026fddd4064a22084f36d64e72bd6de80bb
Bruno Afonso <brunohafonso@gmail.com>
2021-12-14 00:14:49 -0300

    test: add the config of test to run on commits

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   '**/*.ts?(x)': (filenames) =>
-    `next lint --fix --file ${filenames
+    [`next lint --fix --file ${filenames
       .map((file) => file.split(process.cwd())[1])
-      .join(' --file ')}`,
+      .join(' --file ')}`, `jest --maxWorkers=50% --bail --findRelatedTests ${filenames
+        .join(' ')}`],
 }
